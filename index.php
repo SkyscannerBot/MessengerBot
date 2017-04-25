158dfab36f373de0892542bc97569284e2a995c0
Mustafa Tıkır <tikir@itu.edu.tr>
2017-04-25 14:26:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ $printObj = 'The flight choosen for you: \n';
 $printObj = 'Departure Airport: ' .$obj->Places[1]->Name . '\n';
 $printObj .= 'Arrival Airport: ' .$obj->Places[0]->Name . '\n';
 $printObj .= 'Date: ' .strtok($obj->Quotes[0]->OutboundLeg->DepartureDate , 'T') . '\n';
-$printObj .= 'Price: ' .((int)$obj->Quotes[0]->MinPrice * 2) . ' ' .$obj->Currencies[0]->Code . '\n';
+$printObj .= 'Price: ' .$obj->Quotes[0]->MinPrice . ' ' .$obj->Currencies[0]->Code . '\n';
 $printObj .= 'Arrival Airport: ' .$obj->Places[0]->Name . '\n';
 $printObj .= 'Carrier: ' .$obj->Carriers[0]->Name . '\n';
 
