3c27e0ec2c77144b29a67a5de4d7089aa0672bca
Mustafa Tıkır <tikir@itu.edu.tr>
2017-04-25 15:18:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $content = curl_exec($ch);
 $obj = json_decode($content);
 
 $printObj = 'The flight choosen for you:\n';
-if($obj->Places[0]->Name < $obj->Places[1]->Name){
+if($obj->Places[0]->Name > $obj->Places[1]->Name){
 $printObj .= 'Departure Airport: ' .$obj->Places[0]->Name . '\n';
 $printObj .= 'Arrival Airport: ' .$obj->Places[1]->Name . '\n';
 }
